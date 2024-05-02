3151a5b6f87d13e870b40e5e8f1ae8f6481392b7
Arphan Kabak Junior Bodian <127463915+daiki91@users.noreply.github.com>
2024-05-02 15:28:30 +0000

    mobileSaladier

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const randomListView(title: 'Laabb_Saladier'),
+    );
+  }
+}
+
+class randomListView extends StatefulWidget {
+  const randomListView({super.key, required this.title});
+  final String title;
+  @override
+  State<randomListView> createState() => _randomListViewState();
+}
+
+class _randomListViewState extends State<randomListView> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'clique sur le "+" connard:',
+            ),
+            Text(
+              '$_counter',
+              //taille du nombre a l'affichage
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
